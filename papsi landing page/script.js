58ba6caa71e2c6db0b14a215275099e487c8f192
nirmal-gope <nirmal.gope.fr@gmail.com>
2021-01-30 22:37:31 +0100

    new commit

---
+++ papsi landing page/script.js
@@ -0,0 +1,9 @@
+
+function slider(imgSlide){
+    document.querySelector('.pepsi').src = imgSlide;
+}
+function bg(bgColor){
+    const container = document.querySelector('.container');
+    container.style.background = bgColor;
+
+}
